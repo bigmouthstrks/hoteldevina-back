3c63bff829b0c1b63bad8d707fae9b3f2e5b0cab
Benjamin Caceres <benjamin.caceres.ra@gmail.com>
2025-02-08 21:00:23 -0300

    fix: typo in file name worker-messages

---
+++ src/constants/worker-messages.ts
@@ -0,0 +1,17 @@
+export enum WorkerControllerMessages {
+    GET_WORKER_SUCCESS = "Trabajador encontrado con éxito",
+    GET_WORKER_ERROR = "No se pudo encontrar el trabajador",
+    CREATE_WORKER_SUCCESS = "Trabajador registrado con éxito",
+    CREATE_WORKER_ERROR = "No se pudo registrar el trabajador",
+    UPDATE_WORKER_SUCCESS = "Trabajador actualizado con éxito",
+    UPDATE_WORKER_ERROR = "No se pudo actualizar el trabajador",
+    DELETE_WORKER_SUCCESS = "Trabajador eliminado con éxito",
+    DELETE_WORKER_ERROR = "No se pudo eliminar el trabajador"
+};
+
+export enum WorkerRepositoryMessages {
+    GET_WORKER_ERROR = "No se pudo encontrar el trabajador",
+    CREATE_WORKER_ERROR = "No se pudo registrar el trabajador",
+    UPDATE_WORKER_ERROR = "No se pudo actualizar el trabajador",
+    DELETE_WORKER_ERROR = "No se pudo eliminar el trabajador"
+}
