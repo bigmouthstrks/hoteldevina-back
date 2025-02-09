6b9e63f8028b3cc5681446553a21034047d1dfce
Matias Gaete <matias.gaete@smithcommerce.com>
2025-02-09 11:41:50 -0300

    added missing exception for workerRepository

--- src/repository/worker.repository.ts
+++ src/repository/worker.repository.ts
@@ -1,23 +1,44 @@
 import { Worker, PrismaClient } from "@prisma/client";
 import { WorkerData } from "../models/worker";
+import { WorkerRepositoryMessages } from "../constants/worker-messages";
 
 const prisma = new PrismaClient();
 
 class WorkerRepository {
     async getWorker(workerId: number): Promise<Worker | null> {
-        return await prisma.worker.findUnique({ where: { id: workerId } });
+        try {
+            const worker = await prisma.worker.findUnique({ where: { id: workerId } });
+            if (!worker) {
+                throw new Error(WorkerRepositoryMessages.GET_WORKER_ERROR)
+            }
+            return worker;
+        } catch (error) {
+            throw new Error(WorkerRepositoryMessages.GET_WORKER_ERROR)
+        }
     }
 
     async createWorker(workerData: WorkerData): Promise<Worker> {
-        return await prisma.worker.create({ data: workerData });
+        try {
+            return await prisma.worker.create({ data: workerData });
+        } catch (error) {
+            throw new Error(WorkerRepositoryMessages.CREATE_WORKER_ERROR)
+        }
     }
 
     async updateWorker(workerData: WorkerData): Promise<Worker> {
-        return await prisma.worker.update({ where: { id: workerData.id }, data: workerData });
+        try {
+            return await prisma.worker.update({ where: { id: workerData.id }, data: workerData });
+        } catch (error) {
+            throw new Error(WorkerRepositoryMessages.UPDATE_WORKER_ERROR)
+        }
     }
 
     async deleteWorker(workerId: number): Promise<Worker> {
-        return await prisma.worker.delete({ where: { id: workerId } });
+        try {
+            return await prisma.worker.delete({ where: { id: workerId } });
+        } catch (error) {
+            throw new Error(WorkerRepositoryMessages.DELETE_WORKER_ERROR)
+        }
     }
 }
 
