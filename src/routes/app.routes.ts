886dae6b698a7781426e8785af712a6f6d586bc0
Benjamin Caceres <benjamin.caceres.ra@gmail.com>
2025-02-24 23:43:56 -0300

    feat: consume reservation routes in app routes

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -3,12 +3,14 @@ import passengerRouter from './passenger.routes';
 import roomRouter from './room.routes';
 import roomTypeRouter from './room-type.routes';
 import authRouter from './auth.routes';
+import reservationRoutes from './reservation.routes';
 
 const useRoutes = (app: Express): void => {
     app.use(authRouter);
     app.use(passengerRouter);
     app.use(roomRouter);
     app.use(roomTypeRouter);
+    app.use(reservationRoutes);
 };
 
 export default useRoutes;
