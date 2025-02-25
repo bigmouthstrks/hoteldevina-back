b887220d748d6a47b955bee101a726d418c85153
Benjamin Caceres <benjamin.caceres.ra@gmail.com>
2025-02-24 23:42:44 -0300

    feat: update room-type routes

--- src/routes/room-type.routes.ts
+++ src/routes/room-type.routes.ts
@@ -1,11 +1,12 @@
-import { Router } from "express"
-import roomTypeController from "../controllers/room-type.controller"
+import { Router } from 'express';
+import roomTypeController from '../controllers/room-type.controller';
 
 const router = Router();
 
-router.get("/room-types/:roomTypeId", roomTypeController.getRoomType);
-router.post("/room-types", roomTypeController.createRoomType);
-router.put("/room-types", roomTypeController.updateRoomType);
-router.delete("/room-types/:roomTypeId", roomTypeController.deleteRoomType);
+router.get('/room-types', roomTypeController.getRoomTypes);
+router.get('/room-types/:roomTypeId', roomTypeController.getRoomType);
+router.post('/room-types', roomTypeController.createRoomType);
+router.put('/room-types', roomTypeController.updateRoomType);
+router.delete('/room-types/:roomTypeId', roomTypeController.deleteRoomType);
 
 export default router;
