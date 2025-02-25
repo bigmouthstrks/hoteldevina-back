ad956ede43822ba77a80eecec8ed54a0627b1f39
Benjamin Caceres <benjamin.caceres.ra@gmail.com>
2025-02-24 23:43:29 -0300

    feat: add reservation routes

---
+++ src/routes/reservation.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import reservationController from '../controllers/reservation.controller';
+
+const router = Router();
+
+router.get('/reservations/:id', reservationController.getReservationById);
+router.post('/reservations/simulate', reservationController.getSimulation);
+router.post('/reservations/create', reservationController.createReservation);
+router.get('/reservations/status/:id', reservationController.getReservationsByStatusId);
+router.put('/reservations/', reservationController.updateReservationStatus);
+
+export default router;
