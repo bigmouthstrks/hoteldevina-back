dcb3ffec3bce5d9b59f6a31ac47beaac892a84ed
Matias Gaete <matias.gaete@smithcommerce.com>
2025-02-27 10:25:43 -0300

    added allowedorigins to cors

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,20 @@ import useRoutes from './routes/app.routes';
 import cors from 'cors';
 
 const app = express();
-app.use(cors());
-
+// allowedOrigins cuenta con las URL's separadas por "," que permite acceder el backend
+// vienen en formato string en .env ALLOWED_ORIGINS="www.url.com,www.test.com" 
+const allowedOrigins = process.env?.ALLOWED_ORIGINS;
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins?.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Origen no permitido por CORS'));
+            }
+        },
+    })
+);
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
